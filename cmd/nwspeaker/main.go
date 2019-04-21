de9f43a122ab381bb28fac37751f80f846fd086f
Masato Yamazaki <mas9612@gmail.com>
2019-04-21 15:42:49 +0900

    remove unused code

--- cmd/nwspeaker/main.go
+++ cmd/nwspeaker/main.go
@@ -4,18 +4,13 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/mas9612/nwspeaker/pkg/command"
 	"github.com/mitchellh/cli"
 )
 
 func main() {
 	c := cli.NewCLI("nwspeaker", "0.1")
 	c.Args = os.Args[1:]
-	c.Commands = map[string]cli.CommandFactory{
-		"arp": func() (cli.Command, error) {
-			return &command.ArpResolverCommand{}, nil
-		},
-	}
+	c.Commands = map[string]cli.CommandFactory{}
 
 	exitStatus, err := c.Run()
 	if err != nil {
